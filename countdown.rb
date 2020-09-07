fb2902a4f788013d6ee9835c03a481e50bc5ce54
Alla Kuzminova <alchik@gmail.com>
2020-09-07 00:40:37 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep(1.seconds)
+  end
+  "HAPPY NEW YEAR!"
+end
